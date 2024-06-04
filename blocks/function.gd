a84b17d30b595a3216f50e1c47e7164cd5fd1e3a
justinzapanta <zapantajustin5@gmail.com>
2024-06-05 00:11:04 +0800

    walk

---
+++ blocks/function.gd
@@ -0,0 +1,18 @@
+extends Node
+
+func walk():
+	print('walk')
+	
+func jump():
+	print('jump')
+	
+func attack():
+	print('attack')
+	
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
